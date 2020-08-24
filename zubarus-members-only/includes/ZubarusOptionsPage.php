d22b3987846c333c19574efa9835cb51be3064e1
Alex Thomassen <alex@thomassen.xyz>
2020-08-24 15:40:11 +0200

    [Members Only] Add incomplete fields for adding/removing pages

--- zubarus-members-only/includes/ZubarusOptionsPage.php
+++ zubarus-members-only/includes/ZubarusOptionsPage.php
@@ -15,9 +15,25 @@ function zub_render_options_page()
     <form action="options.php" method="post">
         <?php
         settings_fields($optionNames['pages_no_access']);
-        do_settings_sections('zubarus_members_only');
+        do_settings_sections('zubarus_members_only_text');
         ?>
         <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save'); ?>" />
+    </form>
+    <hr style="margin-top: 2em;" />
+    <form action="options.php" method="post">
+        <?php
+        settings_fields($optionNames['pages']);
+        do_settings_sections('zubarus_members_only_add_restricted_page');
+        ?>
+        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Add'); ?>" />
+    </form>
+    <hr style="margin-top: 2em;" />
+    <form action="options.php" method="post">
+        <?php
+        settings_fields($optionNames['pages']);
+        do_settings_sections('zubarus_members_only_del_restricted_page');
+        ?>
+        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Remove'); ?>" />
     </form>
 <?php
 }
@@ -42,6 +58,74 @@ function zub_members_only_option_restricted()
 }
 
 /**
+ * Short description for adding a restricted page
+ */
+function zub_members_only_add_restricted_page_text()
+{
+    printf('<p>%s</p>', __('Add a new page that should be restricted to verified members only.', 'zubarus-members-only'));
+}
+
+/**
+ * Retrieves restricted pages via the options API
+ * Then gets all the Wordpress posts that aren't
+ * restricted yet.
+ *
+ * Unrestricted pages will be listed, so that they can be added
+ * as restricted pages (if that's wanted).
+ */
+function zub_members_only_add_restricted_page()
+{
+    $options = ZubarusOptions::getDefaultOptions();
+    $restrictedPages = zub_get_option('pages');
+
+    printf('<select id="zubarus_members_only_add_restricted_page" name="%s">', $options['pages']['name']);
+    printf('<option>%s</option>', __('&mdash; Select &mdash;'));
+
+    $getPosts = get_posts([
+        'numberposts' => -1,
+        'exclude' => $restrictedPages,
+    ]);
+
+    foreach ($getPosts as $post) {
+        printf('<option value="%s">%s</option>', $post->ID, $post->post_title);
+    }
+
+    print('</select>');
+}
+
+/**
+ * Short description for unrestricting a page
+ */
+function zub_members_only_del_restricted_page_text()
+{
+    printf('<p>%s</p>', __('Remove a page from the list of restricted pages and make it available to the public.', 'zubarus-members-only'));
+}
+
+/**
+ * Retrieves restricted pages via the options API
+ * Then makes sure those posts exist before listing them.
+ */
+function zub_members_only_del_restricted_page()
+{
+    $options = ZubarusOptions::getDefaultOptions();
+    $restrictedPages = zub_get_option('pages');
+
+    printf('<select id="zubarus_members_only_add_restricted_page" name="%s">', $options['pages']['name']);
+    printf('<option>%s</option>', __('&mdash; Select &mdash;'));
+
+    $getPosts = get_posts([
+        'numberposts' => -1,
+        'include' => $restrictedPages,
+    ]);
+
+    foreach ($getPosts as $post) {
+        printf('<option value="%s">%s</option>', $post->ID, $post->post_title);
+    }
+
+    print('</select>');
+}
+
+/**
  * Register options fields.
  */
 function zub_register_options_page()
@@ -51,8 +135,27 @@ function zub_register_options_page()
         'type' => 'string',
     ]);
 
-    add_settings_section('zubarus-members-only-message', __('Settings'), 'zub_members_only_text', 'zubarus_members_only');
-    add_settings_field('zubarus_members_only_restricted_message', __('Message to display on restricted pages.', 'zubarus-members-only'), 'zub_members_only_option_restricted', 'zubarus_members_only', 'zubarus-members-only-message');
+    register_setting($optionNames['pages'], $optionNames['pages'], [
+
+    ]);
+
+    /**
+     * Fields for updating "members-only" replacement text.
+     */
+    add_settings_section('zubarus-members-only-message', __('Settings'), 'zub_members_only_text', 'zubarus_members_only_text');
+    add_settings_field('zubarus_members_only_restricted_message', __('Message to display on restricted pages.', 'zubarus-members-only'), 'zub_members_only_option_restricted', 'zubarus_members_only_text', 'zubarus-members-only-message');
+
+    /**
+     * Fields for adding a new restricted page.
+     */
+    add_settings_section('zubarus-members-only-add-restricted-page', __('Restricted pages &mdash; Add restricted page', 'zubarus-members-only'), 'zub_members_only_add_restricted_page_text', 'zubarus_members_only_add_restricted_page');
+    add_settings_field('zubarus_members_only_add_restricted_page', __('Page that should be restricted to members', 'zubarus-members-only'), 'zub_members_only_add_restricted_page', 'zubarus_members_only_add_restricted_page', 'zubarus-members-only-add-restricted-page');
+
+    /**
+     * Fields for removing a restricted page.
+     */
+    add_settings_section('zubarus-members-only-del-restricted-page', __('Restricted pages &mdash; Remove restricted page', 'zubarus-members-only'), 'zub_members_only_del_restricted_page_text', 'zubarus_members_only_del_restricted_page');
+    add_settings_field('zubarus_members_only_del_restricted_page', __('Page that should be unrestricted and made public', 'zubarus-members-only'), 'zub_members_only_del_restricted_page', 'zubarus_members_only_del_restricted_page', 'zubarus-members-only-del-restricted-page');
 }
 add_action('admin_init', 'zub_register_options_page');
 
