787c157936b1148c6028e1d7e047dcb04c3840ab
Alex Thomassen <alex@thomassen.xyz>
2020-08-31 11:29:33 +0200

    [Members Only] Sanitize add/remove restricted pages, show post status in dropdown lists

---
+++ zubarus-members-only/README.md
@@ -0,0 +1,19 @@
+# Zubarus - Members Only
+
+The Zubarus - Members Only plugin for Wordpress allows you to restrict pages/posts to members that have verified their membership via the Zubarus API.
+
+Guests that have not verified their membership will only see a placeholder text, which can be edited via the Wordpress dashboard.
+
+**Important note**: Currently only the _content_ of the page is replaced with the placeholder text. The title of the page is untouched and will be displayed as normal to anyone.  
+Restricted pages are also shown as norml to users that are logged into Wordpress normally and have the permissions to edit any posts or pages (editors, administrators etc.).
+
+## Requirements
+
+- A Wordpress site
+- Zubarus API credentials
+
+## Translations
+
+Currently the plugin supports English and Norwegian (bokmål).  
+Please note that the translations only affect the Wordpress dashboard / control panel and not the public-facing pages.  
+For instance; The "Members Only" placeholder text is the same for all languages, so write somnething that covers the majority of your userbase.
--- zubarus-members-only/includes/ZubarusOptionsPage.php
+++ zubarus-members-only/includes/ZubarusOptionsPage.php
@@ -58,6 +58,139 @@ function zub_members_only_option_restricted()
 }
 
 /**
+ * Sanitizes *both* REMOVAL and ADDITION of restricted pages.
+ * The input value for the function is the post ID, based
+ * on the `value` attributes of the generated options in the selects.
+ *
+ * It works by:
+ * 1. Retrieving the options from the database
+ * 2. Checks if the post ID is already a restricted page, if it is, then:
+ *  - Remove the post ID from the options
+ *  - Re-index the options array
+ *  - Return the array so Wordpress can serialize the array.
+ * 3. If NOT #2, then it adds the post ID to the restricted page array and returns it.
+ *
+ * This has a minor flaw; If some user decides to be clever and changing one of the "add" option
+ * elements with an existing value, it will implicitly remove it because it's already restricted.
+ * The opposite goes for "removal", setting a value that's NOT restricted in the "removal" section will
+ * implicitly add it because it's not already restricted.
+ *
+ * I consider this a non-issue, since the changes are not devestating.
+ * The main reason it's separated is to show the Wordpress admin(s)
+ * what pages are restricted or not restricted UI-wise.
+ * The "inner workings" are less important.
+ *
+ * @param string $postId
+ *
+ * @return array
+ */
+function zub_members_only_sanitize_restricted_pages($postId)
+{
+    $options = zub_get_option('pages');
+
+    /**
+     * REMOVAL: Remove post from "restricted posts"
+     * and return the new valid options.
+     */
+    if (in_array($postId, $options)) {
+        $optionKey = array_search($postId, $options);
+
+        /**
+         * Maybe redundant check, but just in
+         * case of race conditions occurring...
+         */
+        if ($optionKey !== false) {
+            unset($options[$optionKey]);
+
+            /**
+             * Re-index the numeric keys
+             */
+            $options = array_values($options);
+        }
+
+        return $options;
+    }
+
+    /**
+     * ADDITION: Add post to "restricted posts"
+     */
+    $options[] = $postId;
+    return $options;
+}
+
+/**
+ * PRINTS formatted `<option>` HTML values for the specified posts.
+ * Intended use: `<select>` fields on the Zubarus Members Only options
+ * page for adding/removing restricted pages.
+ *
+ * Expected format: `<option value="$PostId">$PostTitle [$PostStatus]</option>`
+ *
+ * @param WP_Post[] $getPosts
+ *
+ * @return void
+ */
+function zub_print_posts_as_options($getPosts)
+{
+    $postStatuses = get_post_statuses();
+    foreach ($getPosts as $post) {
+        $status = $post->post_status;
+        $statusTrans = $postStatuses[$status] ?? $status;
+
+        /**
+         * For some reason this translation is not included
+         * via `get_post_statuses()`, so we grab it ourselves.
+         */
+        if ($status === 'future') {
+            /**
+             * Normally we'd use `__()` here and be done.
+             *
+             * But without the `post status` context
+             * it would retrieve the wrong translation for
+             * "Scheduled" - Plural instead of singular
+             *
+             * This is only a problem on the admin page.
+             */
+            $statusTrans = _x('Scheduled', 'post status');
+        }
+
+        printf('<option value="%s">%s [%s]</option>', $post->ID, $post->post_title, $statusTrans);
+    }
+}
+
+/**
+ * Helper function for `get_posts()` with relevant pre-filled
+ * values for restricted pages.
+ *
+ * @param array $override Override values for `get_posts()`. Fields such as `include` or `exclude` are expected.
+ *
+ * @return WP_Post[]
+ */
+function zub_get_posts($override = [])
+{
+    $args = [
+        /**
+         * No post limit
+         */
+        'numberposts' => -1,
+
+        /**
+         * Allow posts to be restricted before they're "published".
+         */
+        'post_status' => [
+            'publish',
+            // Scheduled
+            'future',
+            'draft',
+            'pending',
+        ],
+    ];
+
+    $args = array_merge($args, $override);
+
+    return get_posts($args);
+}
+
+/**
  * Short description for adding a restricted page
  */
 function zub_members_only_add_restricted_page_text()
@@ -81,14 +214,11 @@ function zub_members_only_add_restricted_page()
     printf('<select id="zubarus_members_only_add_restricted_page" name="%s">', $options['pages']['name']);
     printf('<option>%s</option>', __('&mdash; Select &mdash;'));
 
-    $getPosts = get_posts([
-        'numberposts' => -1,
+    $getPosts = zub_get_posts([
         'exclude' => $restrictedPages,
     ]);
 
-    foreach ($getPosts as $post) {
-        printf('<option value="%s">%s</option>', $post->ID, $post->post_title);
-    }
+    zub_print_posts_as_options($getPosts);
 
     print('</select>');
 }
@@ -113,14 +243,11 @@ function zub_members_only_del_restricted_page()
     printf('<select id="zubarus_members_only_add_restricted_page" name="%s">', $options['pages']['name']);
     printf('<option>%s</option>', __('&mdash; Select &mdash;'));
 
-    $getPosts = get_posts([
-        'numberposts' => -1,
+    $getPosts = zub_get_posts([
         'include' => $restrictedPages,
     ]);
 
-    foreach ($getPosts as $post) {
-        printf('<option value="%s">%s</option>', $post->ID, $post->post_title);
-    }
+    zub_print_posts_as_options($getPosts);
 
     print('</select>');
 }
@@ -131,12 +258,11 @@ function zub_members_only_del_restricted_page()
 function zub_register_options_page()
 {
     $optionNames = ZubarusOptions::getOptionNames();
-    register_setting($optionNames['pages_no_access'], $optionNames['pages_no_access'], [
-        'type' => 'string',
-    ]);
+    register_setting($optionNames['pages_no_access'], $optionNames['pages_no_access']);
 
     register_setting($optionNames['pages'], $optionNames['pages'], [
-
+        'default' => $optionNames['pages'],
+        'sanitize_callback' => 'zub_members_only_sanitize_restricted_pages',
     ]);
 
     /**
